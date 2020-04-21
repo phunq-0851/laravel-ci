54fe2853fd7cfefc65a6faa820dc6258be75bcbe
quangphu <nguyen.quang.phu@sun-asterisk.com>
2020-04-21 13:56:00 +0700

    heheh

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,8 @@ Route::post('/users/search', 'UserController@search')->name('users.search');
 Route::get('/accessors', function() {
   $user = App\User::find(1);
 
-  return $user->emailname;
-});
+  return $user->emailname
+})
 
 Route::get('/murators', function() {
   $user = App\User::find(1);
